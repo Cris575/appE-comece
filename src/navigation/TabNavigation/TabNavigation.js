778b49d8a6853f0e75d4624afb1e0d22232a061b
Cristian Gonzalez Herrera <cg691539@gmail.com>
2024-06-08 14:45:15 -0600

    commit

--- src/navigation/TabNavigation/TabNavigation.js
+++ src/navigation/TabNavigation/TabNavigation.js
@@ -1,5 +1,9 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View } from "react-native";
+import { Badge } from "react-native-paper";
+import AwesomeIcon from "react-native-vector-icons/FontAwesome";
 import { AuthScreen } from "../../screens/Auth";
+import { styles } from "./TabNavigation.styles";
 import { screensName } from "../../utils";
 import { HomeStack, WishlistStack, CartStack, AccountStack } from "../stacks";
 
@@ -7,7 +11,10 @@ const Tab = createBottomTabNavigator();
 
 export function TabNavigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: (routeStatus) => setIcon(route, routeStatus),
+      })}>
       <Tab.Screen
         name={screensName.home.root}
         component={HomeStack}
@@ -30,4 +37,35 @@ export function TabNavigation() {
       />
     </Tab.Navigator>
   );
+}
+
+function setIcon(route, routeStatus) {
+  const totalProducts = 6;
+  let iconName = "";
+  let color = "#fff";
+
+  if (routeStatus.focused) {
+    color = "#0098d3";
+  }
+
+  if (route.name === screensName.home.root) {
+    iconName = "home";
+  }
+  if (route.name === screensName.wishlist.root) {
+    iconName = "heart";
+  }
+  if (route.name === screensName.account.root) {
+    iconName = "user";
+  }
+
+  if (route.name === screensName.cart.root) {
+    return (
+      <View>
+        <AwesomeIcon name="shopping-cart" color={color} style={styles.icon} />
+        {totalProducts > 0 && <Badge style={styles.totalCart}>{totalProducts}</Badge>}
+      </View>
+    );
+  }
+
+  return <AwesomeIcon name={iconName} color={color} style={styles.icon} />;
 }
